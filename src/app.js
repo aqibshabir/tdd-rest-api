d8b0aaa280341667743f4169cc9e15bfd00d0066
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-01-19 17:44:04 +0000

    test 2 - should test cors middleware to only allow to connect to frontend: http://localhost:3000 - for now

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 export const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,5 +1,6 @@
 import { app } from '../app';
 import { PORT } from '../server';
+import request from 'supertest';
 
 describe('intialising server', () => {
   let server;
@@ -18,5 +19,12 @@ describe('intialising server', () => {
 
   it('should start the server on port 9000', () => {
     expect(server.address().port).toBe(9000);
+  });
+});
+
+describe('CORS middleware', () => {
+  it('should set the access-control-allow-origin header', async () => {
+    const response = await request(app).get('/');
+    expect(response.headers['access-control-allow-origin']).toBe('http://localhost:3000');
   });
 });
